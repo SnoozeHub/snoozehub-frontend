3bd2cd304bd1bd39a6b93aa510defeed3ca48905
Sebastiano Tocci <seba.tocci@gmail.com>
2022-12-25 11:52:19 +0100

    updated vuetify plugin to use latest stable vuetify release

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,8 @@
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
 
-export default defineNuxtPlugin(nuxtApp => {
-    const vuetify = createVuetify({
-        // your config will come here
-    })
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify();
 
-    nuxtApp.vueApp.use(vuetify)
-})
+  nuxtApp.vueApp.use(vuetify);
+});
