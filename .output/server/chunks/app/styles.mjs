1c70d33315d370d0943e7d84bc064752b9c0f01d
Sebastiano Tocci <seba.tocci@gmail.com>
2022-12-25 12:06:25 +0100

    removed .output directory

--- .output/server/chunks/app/styles.mjs
+++
@@ -1,9 +0,0 @@
-const interopDefault = r => r.default || r || [];
-const styles = {
-  entry: () => [],
-  "node_modules/.pnpm/@nuxt+ui-templates@0.4.0/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./_nuxt/error-404-styles.b59a5da0.mjs').then(interopDefault),
-  "node_modules/.pnpm/@nuxt+ui-templates@0.4.0/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./_nuxt/error-500-styles.a5affedd.mjs').then(interopDefault)
-};
-
-export { styles as default };
-//# sourceMappingURL=styles.mjs.map
