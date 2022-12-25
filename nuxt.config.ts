fce88b0628065b54a342ebae0a57a67186b6d55f
Sebastiano Tocci <seba.tocci@gmail.com>
2022-12-25 11:51:48 +0100

    updated various nuxt imports from rc releases to latest stable release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,29 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-import vuetify from 'vite-plugin-vuetify';
+import vuetify from "vite-plugin-vuetify";
 export default defineNuxtConfig({
-    nitro: {
-        serveStatic: true,
-    },
-    imports: {
-        dirs: [
-            "composables",
-            // ... or scan all modules within given directory
-        ]
-    },
-    ssr: false,
-    // head: {
-    //     script: [
-    //         { ssr: false, src: "/grpc-gen/auth-only-service_pb.js" },
-    //         { ssr: false, src: "/grpc-gen/-service_pb.js" },
-    //         { ssr: false, src: "/grpc-gen/common-messages_pb.js" },
-    //         { ssr: false, src: "/grpc-gen/Auth-only-serviceServiceClientPb.ts" },
-    //         { ssr: false, src: "/grpc-gen/Public-serviceServiceClientPb.ts" }
-    //       ]
-    // }
-    // ,
-    build: {
-        transpile: ['grpc-web']
-    },
+  nitro: {
+    serveStatic: true,
+  },
+  imports: {
+    dirs: ["composables"],
+  },
+  ssr: false,
+  build: {
+    transpile: ["grpc-web"],
+  },
 
-    css: ['vuetify/styles'], // vuetify ships precompiled css, no need to import sass
-    vite: {
-        // @ts-ignore
-        // curently this will lead to a type error, but hopefully will be fixed soon #justBetaThings
-        ssr: {
-            noExternal: ['vuetify'], // add the vuetify vite plugin
-        },
+  css: ["vuetify/styles"], // vuetify ships precompiled css, no need to import sass
+  vite: {
+    ssr: {
+      noExternal: ["vuetify"], // add the vuetify vite plugin
     },
-    modules: [
-        // @ts-ignore
-        // this adds the vuetify vite plugin
-        // also produces type errors in the current beta release
-        async (options, nuxt) => {
-            nuxt.hooks.hook('vite:extendConfig', config => config.plugins.push(
-                vuetify()
-            ))
-        }
-    ]
-})
+  },
+  modules: [
+    // @ts-ignore
+    async (options, nuxt) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) =>
+        config.plugins.push(vuetify())
+      );
+    },
+  ],
+});
