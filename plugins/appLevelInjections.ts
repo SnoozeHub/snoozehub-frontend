b23b44e8118f3bd8a4c4557e82dfb0b259335939
Sebastiano Tocci <seba.tocci@gmail.com>
2022-12-27 01:59:49 +0100

    moved injection keys to a separate file to avoid clashes

---
+++ plugins/appLevelInjections.ts
@@ -0,0 +1,11 @@
+export default defineNuxtPlugin((nuxtApp) => {
+//   const userIsAuthenticated = ref(false);
+
+//   function updateUserIsAuthenticated(value: boolean) {
+//     userIsAuthenticated.value = value;
+//   }
+//   nuxtApp.vueApp.provide(userIsAuthenticatedKey, {
+//     userIsAuthenticated,
+//     updateUserIsAuthenticated,
+//   });
+});
