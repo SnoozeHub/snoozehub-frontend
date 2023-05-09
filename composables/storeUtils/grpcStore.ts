e3fa72f7cff34a80edc5cef9122e149ef32ecf42
Sebastiano Tocci <seba.tocci@gmail.com>
2023-05-09 18:07:04 +0200

    fixes

--- composables/storeUtils/grpcStore.ts
+++ composables/storeUtils/grpcStore.ts
@@ -5,7 +5,7 @@ import { PublicServiceClient } from "../grpc_gen/public-service.client";
 
 export const useGrpcStore = defineStore("grpcStore", {
   state: () => ({
-    grpcWebProxyURL: "http://localhost:3000",
+    grpcWebProxyURL: "http://localhost:3010",
     webFetchTransportIsInitiated: false,
     publicServiceClientIsInitiated: false,
     authOnlyServiceClientIsInitiated: false,
