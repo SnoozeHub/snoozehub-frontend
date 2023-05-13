9a47eacd991f1492633cf69b6853649e8b16a16d
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-05-13 15:55:17 +0200

    Update handshake.ts

--- composables/handshake.ts
+++ composables/handshake.ts
@@ -19,15 +19,38 @@ export async function useInitHandshake() {
     method: "eth_requestAccounts",
   });
   const from = accounts[0];
-  const msg = `0x${Buffer.from(nonce, "utf8").toString("hex")}`;
   const signedNonce = await ethereum.request({
     method: "personal_sign",
-    params: [msg, from, "brbrbrbbr"],
+    params: [nonce, from, "brbrbrbbr"],
   });
   console.log("signedNonce", typeof signedNonce, signedNonce);
+
+
+// this is from chatpgpt
+function hexToUint8Array(hexString: string): Uint8Array {
+  const trimmedHex = hexString.startsWith("0x") ? hexString.slice(2) : hexString;
+  const hexLength = trimmedHex.length;
+  
+  if (hexLength % 2 !== 0) {
+    throw new Error("Invalid hexadecimal string: length must be even");
+  }
+  
+  const byteLength = hexLength / 2;
+  const uint8Array = new Uint8Array(byteLength);
+  
+  for (let i = 0; i < byteLength; i++) {
+    const byteHex = trimmedHex.substr(i * 2, 2);
+    const byteValue = parseInt(byteHex, 16);
+    uint8Array[i] = byteValue;
+  }
+  
+  return uint8Array;
+}
+
+
   const auth_request: AuthRequest = {
     nonce,
-    signedNonce: Uint8Array.from(signedNonce),
+    signedNonce: hexToUint8Array(signedNonce),
   };
   console.log("auth_request", auth_request);
   const authOutcome = await publicServiceClient.auth(auth_request);
