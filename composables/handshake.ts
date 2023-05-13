818c1665de3f8e69a70dea5df9b4f80ea8f7dc8d
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2023-05-13 15:56:32 +0200

    Update handshake.ts

--- composables/handshake.ts
+++ composables/handshake.ts
@@ -26,7 +26,7 @@ export async function useInitHandshake() {
   console.log("signedNonce", typeof signedNonce, signedNonce);
 
 
-// this function from chatpgpt
+// this function is from chatpgpt
 function hexToUint8Array(hexString: string): Uint8Array {
   const trimmedHex = hexString.startsWith("0x") ? hexString.slice(2) : hexString;
   const hexLength = trimmedHex.length;
