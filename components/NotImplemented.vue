fd07c7f801e8b452fc4bf66e4b9a467a4f636e3c
Sebastiano Tocci <seba.tocci@gmail.com>
2023-05-24 01:42:26 +0200

    added not implemented message

---
+++ components/NotImplemented.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="not-implemented">
+        <strong>{{ $t('not_implemented') }}</strong>
+        <v-btn @click='$router.back'>{{ $t('go_back') }}</v-btn>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'NotImplemented',
+};
+</script>
+  
+<style scoped>
+.not-implemented {
+    margin-top: 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-weight: bold;
+    font-size: xx-large;
+    color: crimson
+}
+</style>
+  
