59141d83df38aebc478d3c70081d2e159d6b712f
Sebastiano Tocci <seba.tocci@gmail.com>
2023-05-25 01:11:31 +0200

    added default loading of available beds

--- app.vue
+++ app.vue
@@ -6,35 +6,40 @@ import { useTheme } from 'vuetify/lib/framework.mjs';
 const vuetifyTheme = useTheme();
 const { locale } = useI18n();
 import { useI18n } from 'vue-i18n';
+import { useCacheStore } from './composables/storeUtils/cacheStore';
 
 const provider = await detectEthereumProvider();
 
 const sessionStore = useSessionStore();
 sessionStore.setIsWeb3CapableBrowser(provider != null);
+const cacheStore = useCacheStore();
+
 sessionStore.restorePreviousSession();
+cacheStore.retrieveDefaultBedsList();
 vuetifyTheme.global.name.value = sessionStore.getTheme;
 locale.value = sessionStore.getLanguage;
 
-onMounted(() => {
-  useInitGoogleApis();
+onMounted(async () => {
+    useInitGoogleApis();
+
 });
 
 window.addEventListener('beforeunload',
-  sessionStore.saveSession)
-onBeforeUnmount(() => {
-  window.removeEventListener('beforeunload',
     sessionStore.saveSession)
+onBeforeUnmount(() => {
+    window.removeEventListener('beforeunload',
+        sessionStore.saveSession)
 });
 </script>
 
 <template>
-  <div>
-    <v-app>
-      <NuxtLayout class="fill-height">
-        <v-main>
-          <NuxtPage />
-        </v-main>
-      </NuxtLayout>
-    </v-app>
-  </div>
+    <div>
+        <v-app>
+            <NuxtLayout class="fill-height">
+                <v-main>
+                    <NuxtPage />
+                </v-main>
+            </NuxtLayout>
+        </v-app>
+    </div>
 </template>
--- composables/storeUtils/cacheStore.ts
+++ composables/storeUtils/cacheStore.ts
@@ -1,5 +1,7 @@
 import { defineStore } from "pinia";
 import { Bed } from "../grpc_gen/common-messages";
+import { useMessageStore } from "./userMessageStore";
+import { GetBedsRequest } from "../grpc_gen/public-service";
 
 export const useCacheStore = defineStore("cacheStore", {
   state: () => ({
@@ -9,6 +11,15 @@ export const useCacheStore = defineStore("cacheStore", {
     getBedsList: (state) => state.bedsList,
   },
   actions: {
+    async retrieveDefaultBedsList() {
+      this.bedsList = await useFetchBeds(
+        new Date(new Date().setFullYear(2024)),
+        useTomorrowDate(),
+        { latitude: 46.06698, longitude: 11.15503 },
+        [],
+        0
+      );
+    },
     setBedsList(bedsList: Bed[]) {
       this.bedsList = bedsList;
     },
